68d4884a09a110cd9b7dfc1f2c18cb57775fd13f
Teletubisss <natalagorny@gmail.com>
2024-06-15 19:50:56 +0200

    initial commit

---
+++ Assets/Scenes/scripts/Car2Movement.cs
@@ -0,0 +1,28 @@
+
+using UnityEngine;
+
+public class Car2Movement : MonoBehaviour
+
+{
+    [SerializeField] private float Car2speed;
+
+    private Rigidbody2D body;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+        body.transform.position = new Vector2(12, Random.Range(-3.5f, 3.5f));
+    }
+
+    private void Update()
+    {
+        body.velocity = new Vector2(-1 * Car2speed, 0);
+        if (body.transform.position.x < -12)
+        {
+            body.transform.position = new Vector2(12, Random.Range(-3.5f, 3.5F));
+        }
+
+        //if (collision.tag == "Player")
+        //    body.transform.position = new Vector2(12, Random.Range(-3.5f, 3.5f));
+    }
+}
