b346c212a4645c3ed8d297d9378e0ed70007b540
Teletubisss <natalagorny@gmail.com>
2024-06-17 14:37:20 +0200

    Movable and Repetable background :)

---
+++ Assets/Scenes/scripts/BackgroundScroller.cs
@@ -0,0 +1,21 @@
+
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    public float scrollSpeed;
+    public float titleSizeX;
+
+    private Vector3 startPosition;
+
+    void Awake()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        float newPosition = Mathf.Repeat(Time.time * scrollSpeed, titleSizeX);
+        transform.position = startPosition + Vector3.left * newPosition;
+    }
+}
